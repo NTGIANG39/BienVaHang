a34e397d0488fe3643b885f18b1ee98a3addbff4
ntgiang39 <learncodentg@gmail.com>
2023-03-21 13:53:31 +0700

    cccc

--- BaiTap4/Program.cs
+++ BaiTap4/Program.cs
@@ -17,19 +17,19 @@ namespace BaiTap4
             string lop = Console.ReadLine();
 
             Console.WriteLine("Nhap diem trung binh HKI: ");
-            double diemTrungbinhHKI = double.Parse(Console.ReadLine());
+            decimal diemTrungbinhHKI = decimal.Parse(Console.ReadLine());
 
             Console.WriteLine("Nhap diem trung binh HKII: ");
-            double diemTrungbinhHKII = double.Parse(Console.ReadLine());
+            decimal diemTrungbinhHKII = decimal.Parse(Console.ReadLine());
 
             //Công thức tính điêm trung bình cả năm của học sinh
 
-           double diemTrungBinhcanam = (double)(diemTrungbinhHKI + (diemTrungbinhHKII*2))/3;
+           decimal diemTrungBinhcanam = (decimal)(diemTrungbinhHKI + (diemTrungbinhHKII*2))/3;
             
 
             Console.WriteLine("Diem trung binh ca nam cua hoc sinh la: \t {0}",diemTrungBinhcanam);
 
-
+            Console.ReadKey();
 
         }
     }
