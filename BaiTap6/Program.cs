fb8473d98334eae13d16bdfe33b6a1ca9309f189
ntgiang39 <learncodentg@gmail.com>
2023-03-21 15:19:57 +0700

    delete .tostring

--- BaiTap6/Program.cs
+++ BaiTap6/Program.cs
@@ -11,30 +11,40 @@ namespace BaiTap6
         static void Main(string[] args)
         {
 
+            // Console.Write("Nhap vao ngay ban muon: Ngay = ");
+            // string day = Console.ReadLine();
+
+            // Console.Write("Nhap vao thang ban muon: Thang = ");
+            // string month = Console.ReadLine();
+
+            // Console.Write("Nhap vao nam ban muon: Nam = ");
+            // string year =Console.ReadLine();
+
+
+            // DateTime date = DateTime.Parse(day + "/" + month + "/" + year);
+
+
+
+
+            // Console.WriteLine("Ngay hom qua la: \t{0}", date.AddDays(-1).ToString("dd/mm/yyyy"));
+            // Console.WriteLine("Ngay hom nay la: \t{0}", date.ToString("dd/mm/yyyy"));
+            // Console.WriteLine("Ngay hom sau la: \t{0}", date.AddDays(1).ToString("dd/mm/yyyy"));
+
+            // Console.WriteLine("<..........................................................>");
+
+            //Console.ReadKey();
+
+
             Console.Write("Nhap vao ngay ban muon: Ngay = ");
-            string ngay = Console.ReadLine();
+            DateTime date = DateTime.Parse(Console.ReadLine());
 
-            Console.Write("Nhap vao thang ban muon: Thang = ");
-            string thang = Console.ReadLine();
-            
-            Console.Write("Nhap vao nam ban muon: Nam = ");
-            string nam =Console.ReadLine();
-
-            DateTime date = DateTime.Parse(thang + "/" + ngay + "/" + nam);
-                      
-         
-
-
-            Console.WriteLine("Ngay hom qua la: \t{0}",date.AddDays(-1).ToString("dd/mm/yyyy"));
-            Console.WriteLine("Ngay hom nay la: \t{0}", date.ToString("dd/mm/yyyy"));
-            Console.WriteLine("Ngay hom sau la: \t{0}", date.AddDays(1).ToString("dd/mm/yyyy"));
+            Console.WriteLine("Ngay hom qua la: \t{0}", date.AddDays(-1));
+            Console.WriteLine("Ngay hom nay la: \t{0}", date);
+            Console.WriteLine("Ngay hom sau la: \t{0}", date.AddDays(1));
 
             Console.WriteLine("<..........................................................>");
 
-           Console.ReadKey();
-
-
-
+            Console.ReadKey();
 
 
 
